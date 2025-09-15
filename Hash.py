5df8c7fba9de4d8f455cebd83bed609e80a32cee
AnastasijaFed <nastefedorenko@gmail.com>
2025-09-15 15:19:04 +0300

    sukurta funkcija zinutes paruosimui

--- Hash.py
+++ Hash.py
@@ -5,3 +5,12 @@ IV_LEFT = bytes.fromhex("0123456789abcdeffedcba9876543210")
 IV_RIGHT = bytes.fromhex("fedcba98765432100123456789abcdef")
 
 FINAL_CONST = bytes.fromhex('ffffffffffffffffffffffffffffffff')
+
+def pad_message(message: bytes) -> bytes:
+    L = len(message)
+    message_in_bits = L * 8
+    padded = message + b'\x80'
+    num_of_zeros = (16 - (len(padded) + 8) % 16) % 16
+    padded += b'\x00' * num_of_zeros
+    padded += message_in_bits.to_bytes(8, 'big')
+    return padded
