d65b11bc4390e76615f6a26e61edc2c528e39140
Nika <bukolovaite.nika@gmail.com>
2025-09-17 18:23:21 +0300

    Igyvendinta xor funkcija

--- Hash.py
+++ Hash.py
@@ -5,3 +5,8 @@ IV_LEFT = bytes.fromhex("0123456789abcdeffedcba9876543210")
 IV_RIGHT = bytes.fromhex("fedcba98765432100123456789abcdef")
 
 FINAL_CONST = bytes.fromhex('ffffffffffffffffffffffffffffffff')
+
+def xor_bytes(a: bytes, b: bytes) -> bytes:
+    if len(a) != len(b):
+        raise ValueError("Ilgiai turi sutapti")
+    return bytes([x ^ y for x, y in zip(a, b)])
